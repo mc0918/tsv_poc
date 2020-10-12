a3082b8470b30f840c810efd94d5409989bea106
Michael Campbell <mcampbell0918@gmail.com>
2020-10-12 12:21:02 -0500

    started working on cards

--- tsv_ui/components/articlePreview/articlePreview.js
+++ tsv_ui/components/articlePreview/articlePreview.js
@@ -1,15 +1,43 @@
 import {
+  Item,
+  Box,
   Card,
   CardActionArea,
-  CardContent
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Typography,
 } from "@material-ui/core";
 
-export const ArticlePreview = () => {
+const useStyles = makeStyles({
+  root: {
+    backgroundSize: "100% 100%",
+    width: "100%",
+    height: "200px",
+    margin: "10px 0 0 10px",
+    position: "relative",
+    // float: "left",
+  },
+  text: {
+    fontFamily: "sans-serif",
+    fontWeight: 900,
+    color: "white",
+    textTransform: "uppercase",
+    margin: 0,
+    position: "absolute",
+    top: "50%",
+    fontSize: "2rem",
+    transform: "translate(50%, 50%)",
+  },
+});
+export const ArticlePreview = (props) => {
+  const classes = useStyles({ image: props.image });
+
   return (
-    <Card variant="outlined">
-      <CardActionArea>
-        <img style={{height: '100%', width: '100%'}} src="/stoat.jpg"></img>
-      </CardActionArea>
-    </Card>
+    <Box>
+      <CardMedia image={props.image} className={classes.root}>
+        <h2 className={classes.text}>{props.title}</h2>
+      </CardMedia>
+    </Box>
   );
 };
